4fc78a4f2fb437c26215b006ddb563a1ac2e3065
smalllin2022 <123341965+smalllin2022@users.noreply.github.com>
2023-01-23 10:47:45 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,48 @@
+let 方位 = 0
+input.onPinPressed(TouchPin.P0, function () {
+	
+})
+basic.forever(function () {
+    方位 = input.compassHeading()
+    if (方位 < 45) {
+        basic.showLeds(`
+            . . # . .
+            . # . # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    } else if (方位 < 135) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # . #
+            . . . # .
+            . . # . .
+            `)
+    } else if (方位 < 225) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # . # .
+            . . # . .
+            `)
+    } else if (方位 < 315) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # . # # #
+            . # . . .
+            . . # . .
+            `)
+    } else {
+        basic.showLeds(`
+            . . # . .
+            . # . # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    }
+})
